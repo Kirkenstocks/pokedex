aac1fbc4c74b5fe4794550752d6651271febaaa1
Kirk Meyer <kirkmeyer03@gmail.com>
2023-08-24 15:07:42 -0700

    Created initial Pokemon list with 3 Kanto starters

---
+++ js/scripts.js
@@ -0,0 +1,5 @@
+let pokemonList = [
+  {name: 'Bulbasaur', number: 1, height: 0.7, weight: 6.9, types: ['grass', 'poison']},
+  {name: 'Charmander', number: 4, height: 0.6, weight: 8.5, types: 'fire'},
+  {name: 'Squirtle', number: 7, height: 0.5, weight: 9, types: 'water'}  
+];
