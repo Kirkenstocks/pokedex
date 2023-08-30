04613b9f6d82ba2b40bfcd6bf847c5c90256bcf6
Kirk Meyer <kirkmeyer03@gmail.com>
2023-08-30 15:47:27 -0700

    Placed pokemonList in IIFE pokemonRepository

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,3 @@
-let pokemonList = [
-  {name: 'Bulbasaur', number: 1, height: 0.7, weight: 6.9, types: ['grass', 'poison']},
-  {name: 'Charmander', number: 4, height: 0.6, weight: 8.5, types: 'fire'},
-  {name: 'Squirtle', number: 7, height: 0.5, weight: 9, types: 'water'}  
-];
-
 //loop to display name and height of pokemon, with conditional message for height <= 0.5
 function printArrayDetails(list) {
   for (let i = 0; i < list.length; i++) {
@@ -12,6 +6,27 @@ function printArrayDetails(list) {
     if (list[i].height <= 0.5) {
       document.write(' - Aww, look at that little guy!');
     }
+//IIFE Pokemon repository with add & getAll functions
+let pokemonRepository = (function () {
+  let pokemonList = [
+    {name: 'Bulbasaur', number: 1, height: 0.7, weight: 6.9, types: ['Grass', ' Poison']},
+    {name: 'Charmander', number: 4, height: 0.6, weight: 8.5, types: 'Fire'},
+    {name: 'Squirtle', number: 7, height: 0.5, weight: 9, types: 'Water'}  
+  ];
+  
+  function add(pokemon) {
+    pokemonList.push(pokemon);
   }
+  
+  function getAll() {
+    return pokemonList;
+  }
+  
+  return {
+    add: add,
+    getAll: getAll
+  };
+}) ();
+
 }
 printArrayDetails(pokemonList);
