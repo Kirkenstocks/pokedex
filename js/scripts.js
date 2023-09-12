7137bc798a343370bd59e2783f27fbf320e20516
Kirk Meyer <kirkmeyer03@gmail.com>
2023-09-12 15:47:07 -0700

    Defined API to fetch pokemon info from

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,8 @@
 let pokemonRepository = (function () {
   let pokemonList = [];
   
+  let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=151';
+
   //function to add pokemon to repository with conditions
   function add(pokemon) {
     if(typeof pokemon === 'object' &&
